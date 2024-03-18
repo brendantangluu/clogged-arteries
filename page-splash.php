f82afe1ea589126cfdebfc3c26385c6a33ff7eb7
brendantangluu <btangluu@my.bcit.ca>
2024-03-18 10:59:01 -0700

    splash page displays taxonomy terms

--- page-splash.php
+++ page-splash.php
@@ -18,21 +18,32 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
+			echo "<section>";
+				$args = array(
+					'post_type'			=> 'cla-restaurant',
+					'posts_per_page' 	=> -1,
+					'order'				=> 'DESC',
+					'orderby'			=> 'title'
+				);
+					$query = new WP_Query($args);
+					if($query -> have_posts()) :
+						while($query -> have_posts()) :
+							$query -> the_post();
+							?>
+								<?php
+									$id = get_the_id();
+								?>
+									<a href="#<?php echo $id ?>">
+										<?php the_title() ?>
+									</a>
+							<?php
+						endwhile;
+						wp_reset_postdata();
+					endif;
+			echo "</section>";
 		?>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
