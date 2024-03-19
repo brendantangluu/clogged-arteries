1f950ba7a6a58c462fa67b051de1aaf95e2127b7
brendantangluu <btangluu@my.bcit.ca>
2024-03-18 19:45:27 -0700

    food items are now dynamically rendering based on location

--- page-home.php
+++ page-home.php
@@ -28,46 +28,61 @@ get_header();
 
         // Check if $_SESSION['restaurants'] is set and not empty
         if (isset($_SESSION['restaurants']) && !empty($_SESSION['restaurants'])) {
-			$restaurants = get_posts(array('post_type' => 'cla-restaurant'));
-			$restaurant_data = array();
+            $restaurants = get_posts(array('post_type' => 'cla-restaurant'));
+            $restaurant_data = array();
 
-			foreach($restaurants as $restaurant){
-				$restaurant_data[] = $restaurant->post_name;
-			}
-			if(in_array($_SESSION['restaurants'], $restaurant_data)){
-				echo "<section>";
-					$taxonomy = 'cla-menu-categories';
-					$post_type = 'cla-menu';
-					$terms = get_the_terms(get_the_ID(), $taxonomy);
-					
-					if($terms && ! is_wp_error($terms)){
-						foreach($terms as $term){
-							$args = array(
-								'post_type'      => $post_type,
-								'posts_per_page' => -1,
-								'orderby'		 => 'title',
-								'order'			 => 'ASC',
-								'tax_query'		 => array(
-									array(
-										'taxonomy'	=> $taxonomy,
-										'field'		=> 'slug',
-										'terms'  	=> $term->slug
-									)
-								
-								)
-							);
-							$query = new WP_Query($args);
-							if($query -> have_posts()){
-								?> 
-									<h2><?php echo $term->name?></h2>
-								
-								<?php
-							}
-						}
-					}
-				echo "</section>";
-			}
-		}
+            foreach($restaurants as $restaurant){
+                $restaurant_data[] = $restaurant->post_name;
+            }
+            if(in_array($_SESSION['restaurants'], $restaurant_data)){
+                echo "<section>";
+					$menu_category_slug = 'exclusive'; 	
+                    $taxonomy = 'cla-menu-categories';
+                    $post_type = 'cla-menu';
+                    $terms = get_terms(array(
+                        'taxonomy'      => $taxonomy,
+						'slug'          => $menu_category_slug,
+                    ));
+                    if($terms && ! is_wp_error($terms)){
+                        foreach($terms as $term){
+                            $args = array(
+                                'post_type'      => $post_type,
+                                'posts_per_page' => -1,
+                                'orderby'        => 'title',
+                                'order'          => 'ASC',
+                                'tax_query'      => array(
+                                    'relation' => 'AND',
+                                    array(
+                                        'taxonomy' => $taxonomy,
+                                        'field'    => 'slug',
+                                        'terms'    => $term->slug,
+                                    ),
+                                    array(
+                                        'taxonomy' => 'cla-location',
+                                        'field'    => 'slug',
+                                        'terms'    => $_SESSION['restaurants'],
+                                    ),
+                                )
+                            );
+                            $query = new WP_Query($args);
+                            if($query -> have_posts()){
+                                while ($query->have_posts()) {
+                                    $query->the_post();
+									?> 
+										<img src="<?php the_field('food_image') ?>" alt="">
+										<h2><?php the_title(); ?></h2>
+										<p><?php the_field('food_description') ?></p>
+										<aside><?php the_field('food_price')?></aside>
+									<?php
+
+                                }
+                                wp_reset_postdata(); // Reset the post data    
+                            }
+                        }
+                    }
+                echo "</section>";
+            }
+        }
     endif;
     ?>
 
@@ -75,3 +90,4 @@ get_header();
 
 <?php
 get_footer();
+?>
